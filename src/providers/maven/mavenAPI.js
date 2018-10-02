eff3ef38cf5f3ba16601955e68f99c1e2c0c6304
Fernando Crespo <fernando82@gmail.com>
2018-10-01 23:19:51 -0300

    Use repos from pom.xml

--- src/providers/maven/mavenAPI.js
+++ src/providers/maven/mavenAPI.js
@@ -10,31 +10,43 @@ const MAVEN_CENTRAL = "https://repo.maven.apache.org/maven2/"
 
 
 function loadMavenRepositories() {
-  let repositories = []
   const homeDir = os.homedir();
-  return new Promise(function (resolve, reject) {
+  let result = []
+  return Promise.all([new Promise(function (resolve, reject) {
+    let repositories = []
+
     fs.readFile(homeDir + "/.m2/settings.xml", (err, data) => {
       if (err) {
-        resolve([MAVEN_CENTRAL])
-        return
-      }
-      let xml = new xmldoc.XmlDocument(data.toString());
-      let repositoriesXml = xml.descendantWithPath("profiles.profile.repositories").childrenNamed("repository")
+        repositories.push(MAVEN_CENTRAL)
+      } else {
+        let xml = new xmldoc.XmlDocument(data.toString());
+        let repositoriesXml = xml.descendantWithPath("profiles.profile.repositories").childrenNamed("repository")
 
-      repositoriesXml.forEach(repositoryXml => {
-        repositories.push(repositoryXml.childNamed("url").val)
-      })
-
-      if (window.activeTextEditor) {
-        let xmlCurrentPom = new xmldoc.XmlDocument(window.activeTextEditor.document.getText())
-        let repositoriesCurrentPom = xmlCurrentPom.descendantWithPath("repositories").childrenNamed("repository")
-        repositoriesCurrentPom.forEach(element => {
-          repositories.push(element.childNamed("url").val)
+        repositoriesXml.forEach(repositoryXml => {
+          repositories.push(repositoryXml.childNamed("url").val)
         })
       }
       resolve(repositories)
     })
-  });
+  }), new Promise(function (resolve, reject) {
+    let repositories = []
+
+    if (window.activeTextEditor) {
+      let xmlCurrentPom = new xmldoc.XmlDocument(window.activeTextEditor.document.getText())
+      let repositoriesCurrentPom = xmlCurrentPom.descendantWithPath("repositories")
+      if (repositoriesCurrentPom) {
+        repositoriesCurrentPom.eachChild(element => {
+          repositories.push(element.childNamed("url").val)
+        })
+      }
+    }
+    resolve(repositories)
+  })]).then(results => {
+    results.forEach(r => {
+      result = result.concat(r)
+    })
+    return Promise.resolve(result)
+  })
 }
 
 export function mavenGetPackageVersions(packageName) {
@@ -42,8 +54,8 @@ export function mavenGetPackageVersions(packageName) {
     loadMavenRepositories().then((repositories) => {
       let [group, artifact] = packageName.split(':');
       let search = group.replace(/\./g, "/") + "/" + artifact
-
-      Promise.all(repositories.map(element => {
+      let results = []
+      return Promise.all(repositories.map(element => {
         const queryUrl = `${element}${search}/maven-metadata.xml`;
         return httpRequest.xhr({ url: queryUrl })
           .then(response => {
@@ -65,7 +77,12 @@ export function mavenGetPackageVersions(packageName) {
             })
             resolve(versions);
           }).catch(reject);
-      }));
+      })).then(results => {
+        results.forEach(r => {
+          result = result.concat(r)
+        })
+        return Promise.resolve(result)
+      })
     });
   });
 
